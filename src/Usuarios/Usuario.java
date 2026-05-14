2bc351f50b1fd1681151ef50aba1b3c4adbd2745
dami <damianponte@hotmail.es>
2026-05-14 15:12:10 +0200

    modificado parametros, modificado package para evitar fallo;

--- src/Usuarios/Usuario.java
+++ src/Usuarios/Usuario.java
@@ -11,34 +11,39 @@ public abstract class Usuario {
 		this.email = email;
 		this.telefono = telefono;
 	}
+	
 	public String getNombre() {
 		return nombre;
 	}
+	
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+	
 	public String getEmail() {
 		return email;
 	}
+	
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	
 	public String getTelefono() {
 		return telefono;
 	}
+	
 	public void setTelefono(String telefono) {
 		this.telefono = telefono;
 	}
 	
 	
-	public String DatosUsuario() {
+	
+	public String mostrarDatos() {
 		
 		return "nombre: " + nombre +
 				" telefono: " + telefono +
 				"email:" + email;
 	}
-	
-	
-	
+		
 	
 }
