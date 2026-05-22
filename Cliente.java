f95b2b555523d9e985cc7548ea719829e5448c45
dami <damianponte@hotmail.es>
2026-05-22 09:55:29 +0200

    comprobada que aparece la clase en github,

---
+++ Cliente.java
@@ -0,0 +1,35 @@
+package Usuarios;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lineaProducto.Pedido;
+
+public class Cliente extends Usuario {
+
+	private List<Pedido> pedidos = new ArrayList<>();
+	
+	
+	public Cliente(String nombre, String email, String telefono) {
+		super(nombre, email, telefono);
+		
+	}
+
+
+
+	public Pedido crearPedido(String idPedido) {
+		Pedido p=new Pedido (idPedido,this);
+		pedidos.add(p);
+		return p;
+	}
+
+
+
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+
+}
+
+
